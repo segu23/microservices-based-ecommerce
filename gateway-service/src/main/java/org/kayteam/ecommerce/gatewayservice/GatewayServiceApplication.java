c8d2609c3eb753bae2831c19ecadee00d7c4a12f
segu23 <segundoschmidt@gmail.com>
2023-12-07 20:09:02 -0300

    Initial commit

---
+++ gateway-service/src/main/java/org/kayteam/ecommerce/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package org.kayteam.ecommerce.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ registry-service/src/test/java/org/kayteam/ecommerce/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kayteam.ecommerce.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
